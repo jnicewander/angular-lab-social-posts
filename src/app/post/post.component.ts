fd727ddb843ab7c5a3645b72bc09ae5b5b1e7ba9
jnicewander <jnicewander@live.com>
2020-07-13 07:51:01 -0400

    Pre-work commit.

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { listOfPosts } from '../social-posts/social-posts.component';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent implements OnInit {
+export class PostComponent {
+  post = listOfPosts;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
--- src/app/social-posts/social-posts.component.ts
+++ src/app/social-posts/social-posts.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../post';
 
 @Component({
   selector: 'app-social-posts',
   templateUrl: './social-posts.component.html',
   styleUrls: ['./social-posts.component.css']
 })
-export class SocialPostsComponent implements OnInit {
+export class SocialPostsComponent {
+  listOfPosts: Post[] = [
+    {
+      title: "Test Title",
+      thought: "Test thought right here, I wish it was longer."
+    }
+  ];
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
